55de930054ea6ac9f82d03de60ba44300f0065d6
zhangzhen <1071324756@qq.com>
2023-07-03 19:48:13 +0800

    // FIXME 2023年7月3日 下午7:46:30 zhanghen: TODO： 支持ssl nioserver，现在ssl开启后会忽略nioEnable

--- sb_zframework/src/main/java/com/vo/core/ZServer.java
+++ sb_zframework/src/main/java/com/vo/core/ZServer.java
@@ -46,6 +46,7 @@ import com.votool.ze.ZES;
  * @date 2023年6月12日
  *
  */
+// FIXME 2023年7月3日 下午7:46:30 zhanghen: TODO： 支持ssl nioserver，现在ssl开启后会忽略nioEnable
 public class ZServer extends Thread {
 
 	private static final ZLog2 LOG = ZLog2.getInstance();
