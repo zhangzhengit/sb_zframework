b4dc41fcaa7f3cf5854a5eb8acf3c1346c281ef6
zhangzhen <1071324756@qq.com>
2023-07-05 11:31:29 +0800

    ZProperties 添加注释、加入LOG.error 日志

--- sb_zframework/src/main/java/com/vo/conf/ZProperties.java
+++ sb_zframework/src/main/java/com/vo/conf/ZProperties.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
+import com.vo.core.ZLog2;
+
 /**
+ * zframework.properties 配置文件里全部的k=v的值
+ *
  * 加载全部的配置文件的值
  *
  * @author zhangzhen
@@ -14,6 +18,8 @@ import org.apache.commons.configuration.PropertiesConfiguration;
  */
 // FIXME 2023年6月29日 下午11:26:04 zhanghen: 是否支持配置项热更新？改为实时读取配置项？
 public class ZProperties {
+
+	private static final ZLog2 LOG = ZLog2.getInstance();
 
 	public static final ThreadLocal<String> PROPERTIESCONFIGURATION_ENCODING = new ThreadLocal<>();
 	public static final PropertiesConfiguration P;
@@ -28,7 +34,6 @@ public class ZProperties {
 	static {
 		final List<String> plist = ZFrameworkDatasourcePropertiesLoader.PROPERTIES_LIST;
 
-
 		PropertiesConfiguration propertiesConfiguration = null;
 		for (final String pv : plist) {
 			try {
@@ -39,7 +44,8 @@ public class ZProperties {
 		}
 
 		if (propertiesConfiguration == null) {
-			throw new IllegalArgumentException("配置文件不存在");
+			LOG.error("找不到配置文件 [{}", ZFrameworkDatasourcePropertiesLoader.PROPERTIES_NAME);
+			throw new IllegalArgumentException("找不到配置文件 " + ZFrameworkDatasourcePropertiesLoader.PROPERTIES_NAME);
 		}
 
 		PROPERTIESCONFIGURATION_ENCODING.set(propertiesConfiguration.getEncoding());
