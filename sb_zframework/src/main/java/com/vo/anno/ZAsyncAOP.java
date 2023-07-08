4f6c7e133b499b84795cfb444cc44ae1515554d9
zhangzhen <1071324756@qq.com>
2023-07-08 10:34:37 +0800

    加入　＠ZAsync　用在方法上，表示此方法异步执行

---
+++ sb_zframework/src/main/java/com/vo/anno/ZAsyncAOP.java
@@ -0,0 +1,39 @@
+package com.vo.anno;
+
+import com.vo.aop.AOPParameter;
+import com.vo.aop.ZAOP;
+import com.vo.aop.ZIAOP;
+import com.votool.ze.ZE;
+
+/**
+ * @ZAsync 的AOP类，实现异步处理
+ *
+ * @author zhangzhen
+ * @date 2023年7月8日
+ *
+ */
+@ZAOP(interceptType = ZAsync.class)
+public class ZAsyncAOP implements ZIAOP {
+
+	@Override
+	public Object before(final AOPParameter aopParameter) {
+		return null;
+	}
+
+	@Override
+	public Object around(final AOPParameter aopParameter) {
+		final ZE ze = ZAsyncExecutors.getInstance();
+
+		ze.executeInQueue(() -> {
+			final Object invoke = aopParameter.invoke();
+		});
+
+		return null;
+	}
+
+	@Override
+	public Object after(final AOPParameter aopParameter) {
+		return null;
+	}
+
+}
