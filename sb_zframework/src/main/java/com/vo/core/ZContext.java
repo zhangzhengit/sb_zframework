66f8d159be51dea7db5776ed33b6cee1864eb91f
zhangzhen <1071324756@qq.com>
2023-07-09 09:48:01 +0800

    1 删除无用方法、删除注释的代码、ZContext.getBean(name) 改为 ZContext.getBean(Class) 2 ZContext加入方法 getBean(Class)

--- sb_zframework/src/main/java/com/vo/core/ZContext.java
+++ sb_zframework/src/main/java/com/vo/core/ZContext.java
@@ -15,12 +15,17 @@ public class ZContext {
 
 	private static final ConcurrentMap<String, Object> BEAN_MAP = Maps.newConcurrentMap();
 
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(final Class<T> beanClass) {
+		return (T) getBean(beanClass.getCanonicalName());
+	}
+
 	public static Object getBean(final String beanName) {
 		return BEAN_MAP.get(beanName);
 	}
 
-	public static void addBean(final Class<?> clsName, final Object bean) {
-		addBean(clsName.getCanonicalName(), bean);
+	public static void addBean(final Class<?> className, final Object bean) {
+		addBean(className.getCanonicalName(), bean);
 	}
 
 	public static void addBean(final String beanName, final Object bean) {
--- sb_zframework/src/main/java/com/vo/scanner/ZComponentScanner.java
+++ sb_zframework/src/main/java/com/vo/scanner/ZComponentScanner.java
@@ -11,7 +11,6 @@ import com.vo.core.ZContext;
 import com.vo.core.ZLog2;
 import com.vo.core.ZObjectGeneratorStarter;
 import com.vo.core.ZSingleton;
-import com.vo.enums.BeanModeEnum;
 
 /**
  * 扫描 @ZComponent 的类
@@ -28,16 +27,14 @@ public class ZComponentScanner {
 
 		final Map<String, ZClass> map = ZAOPScaner.scanAndGenerateProxyClass1();
 
-		final ServerConfiguration serverConfiguration = (ServerConfiguration) ZContext.getBean(ServerConfiguration.class.getCanonicalName());
+		final ServerConfiguration serverConfiguration = ZContext.getBean(ServerConfiguration.class);
 
-		// 扫描
 		LOG.info("开始扫描带有[{}]注解的类", ZComponent.class.getCanonicalName());
 		final Set<Class<?>> zcSet = ClassMap.scanPackageByAnnotation(serverConfiguration.getScanPackage(), ZComponent.class);
 		LOG.info("扫描到带有[{}]注解的类个数={}", ZComponent.class.getCanonicalName(),zcSet.size());
 		LOG.info("开始给带有[{}]注解的类创建对象",ZComponent.class.getCanonicalName());
 		for (final Class<?> zc : zcSet) {
 			LOG.info("开始给待有[{}]注解的类[{}]创建对象",ZComponent.class.getCanonicalName(),zc.getCanonicalName());
-//			final Object in = ZComponentScanner.getZComponentInstance(zc);
 			final Object object = ZSingleton.getSingletonByClass(zc);
 			LOG.info("给带有[{}]注解的类[{}]创建对象[{}]完成", ZComponent.class.getCanonicalName(),
 					zc.getCanonicalName(), object);
@@ -47,31 +44,14 @@ public class ZComponentScanner {
 			if (proxyClass != null) {
 				final Object newInstance = proxyClass.newInstance();
 				// 放代理类
-//				ZComponentMap.put(newComponent.getClass().getCanonicalName(), newInstance);
-//				ZComponentMap.put(newComponent.getClass().getCanonicalName(), newInstance);
 				ZContext.addBean(newComponent.getClass().getCanonicalName(), newInstance);
 			} else {
-//				ZComponentMap.put(newComponent.getClass().getCanonicalName(), newComponent);
+				// 放原类
 				ZContext.addBean(newComponent.getClass().getCanonicalName(), newComponent);
 			}
 		}
+
 		LOG.info("给带有[{}]注解的类创建对象完成,个数={}", ZComponent.class.getCanonicalName(), zcSet.size());
 	}
 
-
-	public static Object getZComponentInstance(final Class<?> zcClass){
-		final ZComponent zc = zcClass.getAnnotation(ZComponent.class);
-		final BeanModeEnum modeEnum = zc.modeEnum();
-
-		switch (modeEnum) {
-		case SINGLETON:
-			final Object singletonByClass = ZSingleton.getSingletonByClass(zcClass);
-			return singletonByClass;
-
-		default:
-			break;
-		}
-
-		return null;
-	}
 }
