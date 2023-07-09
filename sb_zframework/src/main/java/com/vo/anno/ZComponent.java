48038604237f4e219ac456872a84dcc73d535a6c
zhangzhen <1071324756@qq.com>
2023-07-09 10:19:04 +0800

    删除无用 import

--- sb_zframework/src/main/java/com/vo/anno/ZComponent.java
+++ sb_zframework/src/main/java/com/vo/anno/ZComponent.java
@@ -6,8 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.stereotype.Component;
-
 import com.vo.enums.BeanModeEnum;
 
 /**
