b4b51d849832b7409b4dfcee3787629efd62d055
zhangzhen <1071324756@qq.com>
2023-07-11 11:32:41 +0800

    ZContext 添加javadoc

--- sb_zframework/src/main/java/com/vo/core/ZContext.java
+++ sb_zframework/src/main/java/com/vo/core/ZContext.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ConcurrentMap;
 import com.google.common.collect.Maps;
 
 /**
- * 存取 Bean
+ * 存取 Bean。可使用addBean方法手动注入一个bean让容器管理，使用getBean方法获取一个由容器管理的bean
  *
  * @author zhangzhen
  * @date 2023年7月8日
