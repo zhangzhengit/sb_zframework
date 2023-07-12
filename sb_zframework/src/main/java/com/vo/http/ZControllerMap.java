454f071a0b7ecd28f18ee6abdd5e7299f82c4d91
zhangzhen <1071324756@qq.com>
2023-07-12 12:28:57 +0800

    ZControllerMap.put 添加注释：注册一个接口

--- sb_zframework/src/main/java/com/vo/http/ZControllerMap.java
+++ sb_zframework/src/main/java/com/vo/http/ZControllerMap.java
@@ -10,7 +10,10 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.vo.enums.MethodEnum;
 
+import cn.hutool.core.util.StrUtil;
+
 /**
+ * 存取接口方法
  *
  * @author zhangzhen
  * @date 2023年6月28日
@@ -23,13 +26,19 @@ public class ZControllerMap {
 	static final HashMap<Method, Object> objectMap = Maps.newHashMap();
 	static final HashSet<String> mappingSet = Sets.newHashSet();
 
+	/**
+	 * 注册一个接口
+	 *
+	 * @param methodEnum 接口请求方法，如： MethodEnum.POST
+	 * @param mapping    匹配路径，如：/index
+	 * @param method     具体的接口方法
+	 * @param object     接口方法所在的对象
+	 * @param isRegex    mapping 是否正则表达式
+	 */
 	public synchronized static void put(final MethodEnum methodEnum, final String mapping, final Method method,
 			final Object object, final boolean isRegex) {
-		final boolean add = mappingSet.add(mapping);
-		if (!add) {
-			throw new IllegalArgumentException(
-					"接口方法的 mapping值重复, mapping = " + mapping + "\t" + " method = " + method.getName());
-		}
+
+		checkAPI(methodEnum, mapping, method, object);
 
 		methodPathTable.put(methodEnum, mapping, method);
 
@@ -77,4 +86,31 @@ public class ZControllerMap {
 	public static Boolean getIsregexByMethodEnumAndPath(final Method method, final String path) {
 		return methodIsregexTable.get(method, path);
 	}
+
+	private static void checkAPI(final MethodEnum methodEnum, final String mapping, final Method method,
+			final Object object) {
+		if (methodEnum == null) {
+			throw new IllegalArgumentException(MethodEnum.class.getSimpleName() + " 不能为空");
+		}
+
+		if (StrUtil.isEmpty(mapping)) {
+			throw new IllegalArgumentException("mapping 不能为空");
+		}
+		if (!mapping.startsWith("/")) {
+			throw new IllegalArgumentException("mapping 必须以/开始");
+		}
+		if (method == null) {
+			throw new IllegalArgumentException("method 不能为空");
+		}
+		if (object == null) {
+			throw new IllegalArgumentException("object 不能为空");
+		}
+
+		final boolean add = mappingSet.add(mapping);
+		if (!add) {
+			throw new IllegalArgumentException(
+					"接口方法的 mapping值重复, mapping = " + mapping + "\t" + " method = " + method.getName());
+		}
+	}
+
 }
