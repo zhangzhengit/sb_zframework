b78363909561420fb11a2a4aa62f5e6615b352aa
zhangzhen <1071324756@qq.com>
2023-07-12 12:31:44 +0800

    TODO 使用用户名密码登录（见配置），可以查看运行时间 // 各方法调用次数、执行时长、耗时排序、执行异常信息、可用内存等信息

---
+++ sb_zframework/src/main/java/com/vo/conf/AdminConfiguration.java
@@ -0,0 +1,32 @@
+package com.vo.conf;
+
+import com.vo.anno.ZConfigurationProperties;
+import com.vo.validator.ZMax;
+import com.vo.validator.ZMin;
+import com.vo.validator.ZNotNull;
+import com.vo.validator.ZStartWith;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 管理后台的配置
+ *
+ * @author zhangzhen
+ * @date 2023年7月12日
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ZConfigurationProperties(prefix = "admin")
+public class AdminConfiguration {
+
+	@ZNotNull
+	private String userName;
+
+	@ZNotNull
+	private String password;
+
+}
