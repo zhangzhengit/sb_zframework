653557495bc5de71a5a30c57b5e90b7f2f45fefe
zhangzhen <1071324756@qq.com>
2024-01-12 16:23:28 +0800

    删除已完成的TODO

--- sb_zframework/src/main/java/com/vo/validator/package-info.java
+++ sb_zframework/src/main/java/com/vo/validator/package-info.java
@@ -7,5 +7,3 @@
  */
 package com.vo.validator;
 // FIXME 2023年6月29日 下午11:41:32 zhanghen: 继续写 各种类型的验证器,如： @Min @Max @Length @StartWith 等等
-// FIXME 2023年7月1日 上午4:34:50 zhanghen: TODO 校验：不同的注解，只可以用于指定的类型
-// 如： @Length 只能用于String 和数组类型的字段，否则抛异常
