86fc836416f4f911c6cfb6900a1041f817ad4132
zhangzhen <1071324756@qq.com>
2024-02-04 17:40:50 +0800

    TODO @ZValidated 方式2 还有问题，

--- sb_zframework/src/main/java/com/vo/validator/ZValidated.java
+++ sb_zframework/src/main/java/com/vo/validator/ZValidated.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// FIXME 2024年2月4日 下午5:40:19 zhanghen: 2 貌似还有问题，暂时不要用方式2
 /**
  * 表示启用校验（校验@ZNotNull等用在字段上的注解）。
  * 支持：
@@ -14,6 +15,7 @@ import java.lang.annotation.Target;
  * 	2、@ZComponent 对象的方法的参数上，如：
  * 		public void zv2(final ZVDTO zvdto)
  *	   其中 ZVDTO类上加入本注解，则在接口中调用 zv2方法时会自动校验zvdto上的字段。
+ *
  *
  * @author zhangzhen
  * @date 2023年10月15日
