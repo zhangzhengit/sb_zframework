b9b2fa9fb933beaca6d34ed92279666176ba9526
zhangzhen <1071324756@qq.com>
2025-01-09 22:39:03 +0800

    ZApplication启动成功,耗时X秒 这行重新改为LOG

--- sb_zframework/src/main/java/com/vo/ZApplication.java
+++ sb_zframework/src/main/java/com/vo/ZApplication.java
@@ -70,19 +70,14 @@ public class ZApplication {
 		final long maxMemory = Runtime.getRuntime().maxMemory();
 
 
-		System.out.println("ZApplication启动成功,耗时["+((t2 - t1) / 1000)+"秒],"
-				+ "freeMemory=["+(freeMemory / 1024 / 1024)+"MB],totalMemory=["+(totalMemory / 1024 / 1024)+"MB],"
-				+ "maxMemory=["+(maxMemory / 1024 / 1024)+"MB]");
-
-		//		LOG.info("ZApplication启动成功,耗时[{}]秒,freeMemory=[{}]MB,totalMemory=[{}]MB,maxMemory=[{}]MB",
-		//				((t2 - t1) / 1000),
-		//				freeMemory / 1024 / 1024,
-		//				totalMemory / 1024 / 1024,
-		//				maxMemory / 1024 / 1024);
+		LOG.info("ZApplication启动成功,耗时[{}]秒,freeMemory=[{}]MB,totalMemory=[{}]MB,maxMemory=[{}]MB",
+				((t2 - t1) / 1000),
+				freeMemory / 1024 / 1024,
+				totalMemory / 1024 / 1024,
+				maxMemory / 1024 / 1024);
 
 		final ZApplicationContext context = new ZApplicationContext(ImmutableList.copyOf(scanPackageNameList),
 				httpEnable, args, ZProperties.getInstance());
-		//		httpEnable, args, ZProperties_DELETE.getInstance());
 		return context;
 	}
 
